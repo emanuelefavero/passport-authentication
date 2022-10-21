9e123be8538a24fa145b3310936ed8edde0a330d
emanuelefavero <emafaux89@gmail.com>
2022-10-21 20:11:47 +0200

    add unique rule for username in mongoose User  module

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ db.on('error', console.error.bind(console, 'mongo connection error'))
 const User = mongoose.model(
   'User',
   new Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   })
 )
